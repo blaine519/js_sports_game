343066e62ae770a51eebc4d1bac532b0f851d389
Blaine Presnull <blaine519@gmail.com>
2020-05-07 20:43:05 -0600

    first commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,9 @@ const teamTwoShootButton = document.querySelector("#teamtwo-shoot-button")
 let teamTwoShots = document.querySelector("#teamtwo-numshots")
 let teamTwoGoals = document.querySelector("#teamtwo-numgoals")
 
+const resetButton = document.querySelector("#reset-button")
+let numResets = document.querySelector("#num-resets")
+
 
 teamOneShootButton.addEventListener("click", function() {
     console.log("teamOneShootButton clicked")
@@ -35,7 +38,6 @@ resetButton.addEventListener("click", function() {
     console.log("resetButton clicked")
     let newResetCount = Number(numResets.innerHTML) + 1
     numResets.innerHTML = newResetCount
-
 
     teamOneShots.innerHTML = 0
     teamOneGoals.innerHTML = 0
